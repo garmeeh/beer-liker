c70b1ee423fc69a1b2d77249378c4c434447655b
Gary Meehan <gary@emagine.ie>
2015-07-16 18:06:40 +0100

    Removed unused code,store likes and added simple dialog to show liked beers.

--- app/src/main/java/com/example/garymeehan/beerliker/MainActivity.java
+++ app/src/main/java/com/example/garymeehan/beerliker/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.garymeehan.beerliker;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,25 +23,36 @@ import com.squareup.picasso.Picasso;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity implements View.OnClickListener {
     //View Elements
     TextView textName;
+    ImageView imageView;
     //welcome variables
     private static final String PREFS = "prefs";
     private static final String PREF_NAME = "name";
+    //Shared Prefernces
     SharedPreferences mSharedPreferences;
+    //Simple counter
+    Integer beerCount;
+    //Store Names of Liked Beers
+    List<String> beerList = new ArrayList<>();
 
-    //Final query string, randomaly gets a beer.
+    //query string, randomly gets a beer.
     private static final String QUERY_URL = "http://api.brewerydb.com/v2/beer/random/?key=274b296b2dcfa366021a438566bcff44&format=json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //Kick off welcome message/dialog
         displayWelcome();
         textName = (TextView)findViewById(R.id.text_name);
+        imageView = (ImageView) findViewById(R.id.img_thumbnail);
+        //counter used for likes
+        beerCount = 0;
         //Kick off app with first beer
         queryBeer();
     }
@@ -57,20 +71,50 @@ public class MainActivity extends Activity implements View.OnClickListener {
     }
 
     public void dislike(View v){
-        Log.d("@@@@", "DO NOT LIKE");
         queryBeer();
     }
 
-    public void like(View v){
-        Log.d("@@@@", "LIKE");
+    public void like(View v) throws JSONException {
+
+        if(beerCount <11) {
+            beerCount++;
+            beerList.add(textName.getText().toString());
+        }
+
+        if(beerCount == 10){
+            Toast.makeText(getApplicationContext(), "Nice! 10 Beers Liked!", Toast.LENGTH_LONG).show();
+
+            AlertDialog.Builder builder=new AlertDialog.Builder(this);
+            builder.setTitle("Your Beers!");
+
+            builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+                }
+            });
+
+            builder.setPositiveButton(R.string.cool, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog,int id) {
+                    dialog.cancel();
+                }
+            });
+
+            ListView beerDList = new ListView(this);
+
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                    this,
+                    android.R.layout.simple_list_item_1,
+                    beerList);
+            beerDList.setAdapter(arrayAdapter);
+            builder.setView(beerDList);
+            final Dialog dialog = builder.create();
+            dialog.show();
+        }
+        //Get another beer
         queryBeer();
     }
-
-
 
     private void queryBeer() {
-        final ImageView imageView = (ImageView) findViewById(R.id.img_thumbnail);
-
         // Create client
         AsyncHttpClient client = new AsyncHttpClient();
 
@@ -81,9 +125,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     public void onSuccess(JSONObject jsonObject) {
                         String beerName = null;
                         //show success
-                        Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_LONG).show();
+//                        Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_LONG).show();
 
-                        Log.d("@@@@", String.valueOf(jsonObject.optJSONObject("data")));
                         JSONObject beerObject;
                         //Get beer object from result
                         beerObject = jsonObject.optJSONObject("data");
@@ -98,20 +141,15 @@ public class MainActivity extends Activity implements View.OnClickListener {
                             String iconLabel = null;
                             //get the icon
                             if(newLabels.has("icon")){
-                                Log.d("labels string", String.valueOf(newLabels));
                                 try {
-
                                     iconLabel = newLabels.getString("medium");
                                     Log.d("@@@@", iconLabel);
-
                                 } catch (JSONException e) {
                                     e.printStackTrace();
                                 }
                             }
-
                             Picasso.with(getApplicationContext()).load(iconLabel).placeholder(R.drawable.img_beer_loading).into(imageView);
                         } else {
-
                             // If there is no labels in the object, use a placeholder
                             imageView.setImageResource(R.drawable.img_beer_large);
                         }
@@ -120,15 +158,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
                             textName.setText(beerName);
                         }
                     }
-
                     @Override
                     public void onFailure(int statusCode, Throwable throwable, JSONObject error) {
                         //show failure
                         Toast.makeText(getApplicationContext(), "Error: " + statusCode + " " + throwable.getMessage(), Toast.LENGTH_LONG).show();
-
                         // Log error message
                         Log.e("Query Error", statusCode + " " + throwable.getMessage());
-
                     }
                 });
     }
@@ -141,7 +176,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         String name = mSharedPreferences.getString(PREF_NAME, "");
 
         if (name.length() > 0) {
-            // If the name is valid, display a Toast welcoming them
+            // If the name is valid, display Toast
             Toast.makeText(this, "Welcome back, " + name + "!", Toast.LENGTH_LONG).show();
         }else {
             // otherwise, show a dialog to ask for their name
@@ -168,16 +203,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     Toast.makeText(getApplicationContext(), "Welcome, " + inputName + "!", Toast.LENGTH_LONG).show();
                 }
             });
-
             // Make a "Cancel" button
             // that simply dismisses the alert
             alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-
                 public void onClick(DialogInterface dialog, int whichButton) {}
             });
-
             alert.show();
         }
-    }//end welcome
-
+    }
 }
